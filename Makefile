900f1aefc5825e6ab3202c9fe0954a09bd2314d3
tim-patterson <tim.wanaka@gmail.com>
2012-08-08 21:20:42 -0700

    Changed project to use distutils and created make file for building rpms

---
+++ Makefile
@@ -0,0 +1,13 @@
+rpm:
+	mkdir dist
+ifneq "$(BUILD_NUMBER)" ""
+	python setup.py bdist_rpm --release=$(BUILD_NUMBER)
+else
+	python setup.py bdist_rpm --release=0.`date +'%s'`.test
+endif
+
+clean:
+	python setup.py clean
+	rm -rf build
+	rm -rf dist
+	rm -rf MANIFEST
---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+import os
+
+app_name = 'django-chronograph'
+source_dir = 'src'
+
+packages = []
+data_files = []
+
+for dirpath, dirnames, filenames in os.walk(source_dir):
+    if '__init__.py' in filenames:
+        packages.append('.'.join(dirpath.split(os.sep)[1:]))
+    elif filenames:
+        data_files.append([os.sep.join(dirpath.split(os.sep)[1:]), [os.path.join(dirpath, f) for f in filenames]])
+
+setup(
+    name=app_name,
+    version='0.1.11',
+    package_dir={'':source_dir},
+    description='Django chronograph application.',
+    url='https://github.com/tim-patterson/django-chronograph',
+    author='Weston Nielson',
+    author_email='wnielson@gmail.com',
+    packages = packages,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    data_files = data_files
+)
