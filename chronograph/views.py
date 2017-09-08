dd9cbe56471151dcd32e1868b603b22c134abad4
Ruan Seabra <ruan.seabra@orama.com.br>
2017-09-08 13:08:23 -0300

    Removed superuser need

--- chronograph/views.py
+++ chronograph/views.py
@@ -1,4 +1,5 @@
 from .admin import JobAdmin
+from .constants import JOB_RUN_PERMISSIONS
 from .models import Job
 from django.contrib import admin
 from django.contrib.auth.decorators import user_passes_test
@@ -6,4 +7,6 @@ from django.contrib.auth.decorators import user_passes_test
 
 def job_run(request, pk):
     return JobAdmin(Job, admin.site).run_job_view(request, pk)
-job_run = user_passes_test(lambda user: user.is_superuser)(job_run)
+
+
+job_run = user_passes_test(lambda user: user.has_perms(JOB_RUN_PERMISSIONS))(job_run)
